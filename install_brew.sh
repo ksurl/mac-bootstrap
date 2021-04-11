1fa2b7c6e7c0e95b92fc4f218198345d29a22008
ksurl <ksurl@users.noreply.github.com>
2021-04-11 12:30:08 -0700

    add xcode install

--- install_brew.sh
+++ install_brew.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-xcode-select --install
+xcode-select -p &> /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "xcode cli tools not found. Installing ..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+else
+  echo "Xcode CLI tools OK"
+fi
 
 /bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew update
 
 brew bundle
+
